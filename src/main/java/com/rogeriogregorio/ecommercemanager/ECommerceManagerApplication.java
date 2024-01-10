fcdff13f3e1ab4ec45b534fc692da757e585dcf1
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-01-10 17:07:05 -0300

    Project created

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/ECommerceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.ecommercemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogeriogregorio/ecommercemanager/ECommerceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.ecommercemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
