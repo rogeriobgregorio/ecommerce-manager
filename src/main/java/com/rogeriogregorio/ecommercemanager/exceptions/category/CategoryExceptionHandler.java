c3e07cbeebae1f6c3627e9da7e56bf743c4eec99
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-02-01 18:11:32 -0300

    feat: Add CategoryExceptionHandler class

--- src/main/java/com/rogeriogregorio/ecommercemanager/exceptions/category/CategoryExceptionHandler.java
+++ src/main/java/com/rogeriogregorio/ecommercemanager/exceptions/category/CategoryExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.rogeriogregorio.ecommercemanager.exceptions.order;
+package com.rogeriogregorio.ecommercemanager.exceptions.category;
 
 import com.rogeriogregorio.ecommercemanager.exceptions.StandardError;
 import org.springframework.http.HttpStatus;
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class OrderExceptionHandler {
+public class CategoryExceptionHandler {
 
-    @ExceptionHandler(OrderRepositoryException.class)
-    public ResponseEntity<StandardError> HandleOrderRepositoryException(OrderRepositoryException ex) {
+    @ExceptionHandler(CategoryRepositoryException.class)
+    public ResponseEntity<StandardError> HandleCategoryRepositoryException(CategoryRepositoryException ex) {
 
         StandardError error = new StandardError(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao tentar acessar o repositório", ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
-    @ExceptionHandler(OrderNotFoundException.class)
-    public ResponseEntity<StandardError> handleOrderNotFoundException(OrderNotFoundException ex) {
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<StandardError> handleCategoryNotFoundException(CategoryNotFoundException ex) {
 
-        StandardError error = new StandardError(HttpStatus.NOT_FOUND, "Pedido não encontrado", ex.getMessage());
+        StandardError error = new StandardError(HttpStatus.NOT_FOUND, "Categoria não encontrada", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
