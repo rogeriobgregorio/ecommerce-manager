53f9b92deaa69ed5ce77f58e34ca48d151f82f21
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-02-02 18:22:45 -0300

    feat: Add ProductController class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/controllers/ProductController.java
@@ -0,0 +1,74 @@
+package com.rogeriogregorio.ecommercemanager.controllers;
+
+import com.rogeriogregorio.ecommercemanager.dto.ProductRequest;
+import com.rogeriogregorio.ecommercemanager.dto.ProductResponse;
+import com.rogeriogregorio.ecommercemanager.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping(value = "/products")
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.findAllProducts());
+    }
+
+    @PostMapping(value = "/products")
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest productRequest) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(productService.createProduct(productRequest));
+    }
+
+    @GetMapping(value = "/products/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.findProductById(id));
+    }
+
+    @PutMapping(value = "/products")
+    public ResponseEntity<ProductResponse> updateProduct(@Valid @RequestBody ProductRequest productRequest) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.updateProduct(productRequest));
+    }
+
+    @DeleteMapping(value = "/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+
+        productService.deleteProduct(id);
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+
+    @GetMapping(value = "/products/search")
+    public ResponseEntity<List<ProductResponse>> getProductByName(@RequestParam("name") String name) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.findProductByName(name));
+    }
+}
