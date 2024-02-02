7880e05609b863b4c471bcb1397c90b9710d345f
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-02-02 17:23:33 -0300

    feat: Add ProductConverter class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/util/Impl/ProductConverter.java
@@ -0,0 +1,30 @@
+package com.rogeriogregorio.ecommercemanager.util.Impl;
+
+import com.rogeriogregorio.ecommercemanager.dto.ProductRequest;
+import com.rogeriogregorio.ecommercemanager.dto.ProductResponse;
+import com.rogeriogregorio.ecommercemanager.entities.ProductEntity;
+import com.rogeriogregorio.ecommercemanager.util.Converter;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductConverter implements Converter<ProductRequest, ProductEntity, ProductResponse> {
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public ProductConverter(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public ProductEntity requestToEntity(ProductRequest productRequest) {
+        return modelMapper.map(productRequest, ProductEntity.class);
+    }
+
+    @Override
+    public ProductResponse entityToResponse(ProductEntity productEntity) {
+        return modelMapper.map(productEntity, ProductResponse.class);
+    }
+}
