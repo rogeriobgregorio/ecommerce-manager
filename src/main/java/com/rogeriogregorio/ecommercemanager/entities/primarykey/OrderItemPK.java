204039eb4f539ef29da0b6e4d83a72feecdd6df7
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-02-03 20:17:11 -0300

    feat: Add OrderItemPK class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/entities/primarykey/OrderItemPK.java
@@ -0,0 +1,51 @@
+package com.rogeriogregorio.ecommercemanager.entities.primarykey;
+
+import com.rogeriogregorio.ecommercemanager.entities.OrderEntity;
+import com.rogeriogregorio.ecommercemanager.entities.ProductEntity;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private OrderEntity orderEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductEntity productEntity;
+
+    public OrderEntity getOrderEntity() {
+        return orderEntity;
+    }
+
+    public void setOrderEntity(OrderEntity orderEntity) {
+        this.orderEntity = orderEntity;
+    }
+
+    public ProductEntity getProductEntity() {
+        return productEntity;
+    }
+
+    public void setProductEntity(ProductEntity productEntity) {
+        this.productEntity = productEntity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(orderEntity, that.orderEntity) && Objects.equals(productEntity, that.productEntity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderEntity, productEntity);
+    }
+}
