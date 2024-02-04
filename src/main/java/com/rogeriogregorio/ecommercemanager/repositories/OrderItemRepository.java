2a2bf6ee302741a423dbd1d9e75d1a9f24b6ce31
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-02-03 23:30:24 -0300

    feat: Add OrderItemRepository interface

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.rogeriogregorio.ecommercemanager.repositories;
+
+import com.rogeriogregorio.ecommercemanager.entities.OrderItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItemEntity, Long> {
+}
