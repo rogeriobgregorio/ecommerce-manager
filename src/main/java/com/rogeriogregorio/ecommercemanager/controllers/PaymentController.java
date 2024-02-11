7619a3873995d87245bf4a8a531d674377ef7072
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-02-11 17:44:43 -0300

    feat: Add PaymentController class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/controllers/PaymentController.java
@@ -0,0 +1,67 @@
+package com.rogeriogregorio.ecommercemanager.controllers;
+
+import com.rogeriogregorio.ecommercemanager.dto.requests.PaymentRequest;
+import com.rogeriogregorio.ecommercemanager.dto.responses.PaymentResponse;
+import com.rogeriogregorio.ecommercemanager.services.PaymentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+
+        this.paymentService = paymentService;
+    }
+
+    @GetMapping(value = "/payments")
+    public ResponseEntity<List<PaymentResponse>> getAllPayments() {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(paymentService.findAllPayments());
+    }
+
+    @PostMapping(value = "/payments")
+    public ResponseEntity<PaymentResponse> createPayment(@Valid @RequestBody PaymentRequest paymentRequest) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(paymentService.createPayment(paymentRequest));
+    }
+
+    @GetMapping(value = "/payments/{id}")
+    public ResponseEntity<PaymentResponse> getPaymentById(@PathVariable Long id) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(paymentService.findPaymentById(id));
+    }
+
+    @PutMapping(value = "/payments")
+    public ResponseEntity<PaymentResponse> updatePayment(@Valid @RequestBody PaymentRequest paymentRequest) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(paymentService.updatePayment(paymentRequest));
+    }
+
+    @DeleteMapping(value = "/payments/{id}")
+    public ResponseEntity<Void> deletePayment(@PathVariable Long id) {
+
+        paymentService.deletePayment(id);
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+}
