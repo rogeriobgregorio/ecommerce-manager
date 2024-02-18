23b9de64ec9515007b83e57d261e8c15f64a1694
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-02-17 21:14:10 -0300

    feat: Add test methods in OrderItemImplTest class

--- src/test/java/com/rogeriogregorio/ecommercemanager/services/OrderItemServiceImplTest.java
+++ src/test/java/com/rogeriogregorio/ecommercemanager/services/OrderItemServiceImplTest.java
@@ -7,6 +7,7 @@ import com.rogeriogregorio.ecommercemanager.entities.OrderItemEntity;
 import com.rogeriogregorio.ecommercemanager.entities.ProductEntity;
 import com.rogeriogregorio.ecommercemanager.entities.UserEntity;
 import com.rogeriogregorio.ecommercemanager.entities.enums.OrderStatus;
+import com.rogeriogregorio.ecommercemanager.exceptions.RepositoryException;
 import com.rogeriogregorio.ecommercemanager.repositories.OrderItemRepository;
 import com.rogeriogregorio.ecommercemanager.services.impl.OrderItemServiceImpl;
 import com.rogeriogregorio.ecommercemanager.util.Converter;
@@ -23,9 +24,9 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -110,7 +111,7 @@ public class OrderItemServiceImplTest {
 
     @Test
     @DisplayName("findAllOrderItems - Busca bem-sucedida retorna lista de itens de pedidos vazia")
-    void findAllCategories_SuccessfulSearch_ReturnsEmptyList() {
+    void findAllOrderItems_SuccessfulSearch_ReturnsEmptyList() {
         // Arrange
         List<OrderItemEntity> emptyOrderItemList = new ArrayList<>();
 
@@ -125,4 +126,94 @@ public class OrderItemServiceImplTest {
 
         verify(orderItemRepository, times(1)).findAll();
     }
+
+    @Test
+    @DisplayName("findAllOrderItems - Exceção ao tentar buscar lista de itens de pedidos")
+    void findAllOrderItems_RepositoryExceptionHandling() {
+        // Arrange
+        when(orderItemRepository.findAll()).thenThrow(RuntimeException.class);
+
+        // Act and Assert
+        assertThrows(RepositoryException.class, () -> orderItemService.findAllOrderItems());
+
+        verify(orderItemRepository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("createOrderItem - Criação bem-sucedida retorna item do pedido criado")
+    void createOrderItem_SuccessfulCreation_ReturnsOrderItemResponse() {
+        // Arrange
+        UserEntity u1 = new UserEntity(1L, "João Silva", "joao@email.com", "11912345678", "senha123");
+        OrderEntity o1 = new OrderEntity(1L, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        ProductEntity p1 = new ProductEntity(1L, "Playstation 5", "Video game console", 4099.0, "www.url.com");
+
+        OrderItemRequest orderItemRequest = new OrderItemRequest(o1, p1, 1, 4099.0);
+        OrderItemEntity orderItemEntity = new OrderItemEntity(o1, p1, 1, 4099.0);
+        OrderItemResponse expectedResponse = new OrderItemResponse(o1, p1, 1, 4099.0);
+
+        when(orderItemConverter.requestToEntity(orderItemRequest)).thenReturn(orderItemEntity);
+        when(orderItemConverter.entityToResponse(orderItemEntity)).thenReturn(expectedResponse);
+        when(orderItemRepository.save(orderItemEntity)).thenReturn(orderItemEntity);
+
+        // Act
+        OrderItemResponse actualResponse = orderItemService.createOrderItem(orderItemRequest);
+
+        // Assert
+        assertNotNull(actualResponse, "OrderItemResponse should not be null");
+        assertEquals(expectedResponse, actualResponse, "Expected and actual responses should be qual");
+
+        verify(orderItemConverter, times(1)).requestToEntity(orderItemRequest);
+        verify(orderItemConverter, times(1)).entityToResponse(orderItemEntity);
+        verify(orderItemRepository, times(1)).save(orderItemEntity);
+    }
+
+    @Test
+    @DisplayName("createOrderItem - Exceção no repositório ao tentar criar item do pedido")
+    void createOrderItem_RepositoryExceptionHandling() {
+        // Arrange
+        UserEntity u1 = new UserEntity(1L, "João Silva", "joao@email.com", "11912345678", "senha123");
+        OrderEntity o1 = new OrderEntity(1L, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        ProductEntity p1 = new ProductEntity(1L, "Playstation 5", "Video game console", 4099.0, "www.url.com");
+
+        OrderItemRequest orderItemRequest = new OrderItemRequest(o1, p1, 1, 4099.0);
+        OrderItemEntity orderItemEntity = new OrderItemEntity(o1, p1, 1, 4099.0);
+
+        when(orderItemConverter.requestToEntity(orderItemRequest)).thenReturn(orderItemEntity);
+        when(orderItemRepository.save(orderItemEntity)).thenThrow(RuntimeException.class);
+
+        // Act and Assert
+        assertThrows(RepositoryException.class, () -> orderItemService.createOrderItem(orderItemRequest));
+
+        verify(orderItemConverter, times(1)).requestToEntity(orderItemRequest);
+        verify(orderItemRepository, times(1)).save(orderItemEntity);
+    }
+
+    @Test
+    @DisplayName("findOrderItemById - Busca bem-sucedida retorna item(s) do pedido")
+    void findOrderItemById_SuccessfulSearch_ReturnsOrderItemResponse() {
+        // Arrange
+        UserEntity u1 = new UserEntity(1L, "João Silva", "joao@email.com", "11912345678", "senha123");
+        OrderEntity o1 = new OrderEntity(1L, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        ProductEntity p1 = new ProductEntity(1L, "Playstation 5", "Video game console", 4099.0, "www.url.com");
+
+        OrderItemEntity orderItemEntity = new OrderItemEntity(o1, p1, 1, 4099.0);
+        OrderItemResponse expectedResponse = new OrderItemResponse(o1, p1, 1, 4099.0);
+
+        when(orderItemConverter.entityToResponse(orderItemEntity)).thenReturn(expectedResponse);
+        when(orderItemRepository.findById(1L)).thenReturn(Optional.of(orderItemEntity));
+
+        // Act
+        OrderItemResponse actualResponse = orderItemService.findOrderItemById(1L);
+
+        // Assert
+        assertNotNull(actualResponse, "OrderItemResponse should not be null");
+        assertEquals(expectedResponse, actualResponse, "Expected and actual responses should be equal");
+
+        verify(orderItemConverter, times(1)).entityToResponse(orderItemEntity);
+        verify(orderItemRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("findOrderItemById - Exceção ao tentar buscar categoria inexistente")
+    void findCategory_NotFoundExceptionHandling(){}
 }
