65a4585a6a8d2698de2c5050acffb2687c7401f2
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-03-18 22:03:10 -0300

    feat: Add StockStatus enum class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/entities/enums/StockStatus.java
@@ -0,0 +1,28 @@
+package com.rogeriogregorio.ecommercemanager.entities.enums;
+
+public enum StockStatus {
+
+    AVAILABLE(1),
+    OUT_OF_STOCK(2),
+    PRE_ORDER(3);
+
+    private int code;
+
+    private StockStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static StockStatus valueOf(int code) {
+
+        for (StockStatus value : StockStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("O código de status do estoque é inválido");
+    }
+}
