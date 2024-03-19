d543473d56a8cb5b0382899b06d8ee97166c70af
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-03-18 22:02:20 -0300

    feat: Add MovementType enum class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/entities/enums/MovementType.java
@@ -0,0 +1,27 @@
+package com.rogeriogregorio.ecommercemanager.entities.enums;
+
+public enum MovementType {
+
+    ENTRANCE(1),
+    EXIT(2);
+
+    private int code;
+
+    private MovementType(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static MovementType valueOf(int code) {
+
+        for (MovementType value : MovementType.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("O código de movimentação do estoque é inválido");
+    }
+}
