38a1466a40cde14e7be5139ee1c3f804c2365990
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-03-21 22:22:01 -0300

    feat: Add StockMovementRequest dto class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/dto/requests/StockMovementRequest.java
@@ -0,0 +1,76 @@
+package com.rogeriogregorio.ecommercemanager.dto.requests;
+
+import com.rogeriogregorio.ecommercemanager.entities.enums.MovementType;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class StockMovementRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private Long inventoryItemId;
+    private Integer movementType;
+    private Integer quantityMoved;
+
+    public StockMovementRequest() {
+    }
+
+    public StockMovementRequest(Long inventoryItemId, MovementType movementType, Integer quantityMoved) {
+        this.inventoryItemId = inventoryItemId;
+        setMovementType(movementType);
+        this.quantityMoved = quantityMoved;
+    }
+
+    public StockMovementRequest(Long id, Long inventoryItemId, MovementType movementType, Integer quantityMoved) {
+        this.id = id;
+        this.inventoryItemId = inventoryItemId;
+        setMovementType(movementType);
+        this.quantityMoved = quantityMoved;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getInventoryItemId() {
+        return inventoryItemId;
+    }
+
+    public void setInventoryItemId(Long inventoryItemId) {
+        this.inventoryItemId = inventoryItemId;
+    }
+
+    public MovementType getMovementType() {
+        return MovementType.valueOf(movementType);
+    }
+
+    public void setMovementType(MovementType movementType) {
+
+        if (movementType == null) {
+            throw new IllegalArgumentException("O status de movimentação do item do estoque não pode ser nulo");
+        }
+
+        this.movementType = movementType.getCode();
+    }
+
+    public Integer getQuantityMoved() {
+        return quantityMoved;
+    }
+
+    public void setQuantityMoved(Integer quantityMoved) {
+        this.quantityMoved = quantityMoved;
+    }
+
+    @Override
+    public String toString() {
+        return "[Movimentação do estoque: id= " + id + ", inventoryItem= " + inventoryItemId +
+                ", movementType= " + movementType + ", quantityMoved= " + quantityMoved +"]";
+    }
+}
