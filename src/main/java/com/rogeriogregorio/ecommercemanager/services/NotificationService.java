97913999be5e9d267ce488a77f030f549a4ed3de
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-04-25 22:37:24 -0300

    feat: Add NotificationService class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/services/NotificationService.java
@@ -0,0 +1,21 @@
+package com.rogeriogregorio.ecommercemanager.services;
+
+import com.rogeriogregorio.ecommercemanager.dto.requests.NotificationRequest;
+import com.rogeriogregorio.ecommercemanager.dto.responses.NotificationResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface NotificationService {
+
+    Page<NotificationResponse> findAllNotifications(Pageable pageable);
+
+    NotificationResponse createNotification(NotificationRequest notificationRequest);
+
+    NotificationResponse findNotificationById(Long id);
+
+    NotificationResponse updateNotification(NotificationRequest notificationRequest);
+
+    void deleteNotification(Long id);
+}
