9380ebde41c6bdf44af6ebd9a1c59e7bce3cce38
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-04-27 04:38:30 -0300

    feat: Add ProductDiscountService class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/services/ProductDiscountService.java
@@ -0,0 +1,22 @@
+package com.rogeriogregorio.ecommercemanager.services;
+
+import com.rogeriogregorio.ecommercemanager.dto.requests.ProductDiscountRequest;
+import com.rogeriogregorio.ecommercemanager.dto.responses.ProductDiscountResponse;
+import com.rogeriogregorio.ecommercemanager.entities.ProductDiscount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductDiscountService {
+
+    Page<ProductDiscountResponse> findAllProductDiscounts(Pageable pageable);
+
+    ProductDiscountResponse createProductDiscount(ProductDiscountRequest productDiscountRequest);
+
+    ProductDiscountResponse findProductDiscountResponseById(Long id);
+
+    ProductDiscount findProductDiscountById(Long id);
+
+    ProductDiscountResponse updateProductDiscount(ProductDiscountRequest productDiscountRequest);
+
+    void deleteProductDiscount(Long id);
+}
