bef0f85b518ded0e4f425fd114d3f7343af878f2
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-04-29 18:57:46 -0300

    feat: Add ProductReviewRepository class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/repositories/ProductReviewRepository.java
@@ -0,0 +1,9 @@
+package com.rogeriogregorio.ecommercemanager.repositories;
+
+import com.rogeriogregorio.ecommercemanager.entities.ProductReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+}
