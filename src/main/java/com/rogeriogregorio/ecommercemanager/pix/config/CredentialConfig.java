89ce702dac8a41787c8f6424ad9746a7a3023f48
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-05-19 18:45:13 -0300

    feat: Add CredentialConfig class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/pix/config/CredentialConfig.java
@@ -0,0 +1,64 @@
+package com.rogeriogregorio.ecommercemanager.pix.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "pix")
+public class CredentialConfig {
+
+    private String clientId;
+    private String clientSecret;
+    private String certificate;
+    private boolean sandbox;
+    private boolean debug;
+    private String keyEVP;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    public boolean isSandbox() {
+        return sandbox;
+    }
+
+    public void setSandbox(boolean sandbox) {
+        this.sandbox = sandbox;
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    public String getKeyEVP() {
+        return keyEVP;
+    }
+
+    public void setKeyEVP(String keyEVP) {
+        this.keyEVP = keyEVP;
+    }
+}
