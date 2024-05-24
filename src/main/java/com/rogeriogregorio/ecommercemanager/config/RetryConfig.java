63cb13594787b27794201379297fcda392bdecdd
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-05-24 00:41:24 -0300

    feat: Add RetryConfig class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/config/RetryConfig.java
@@ -0,0 +1,10 @@
+package com.rogeriogregorio.ecommercemanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+}
+
