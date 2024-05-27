f27fa45d2c099c271733d6e8188e6de434f127bd
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-05-26 22:55:54 -0300

    feat: Add PixEVPKeyDTO class

---
+++ src/main/java/com/rogeriogregorio/ecommercemanager/dto/PixEVPKeyDTO.java
@@ -0,0 +1,34 @@
+package com.rogeriogregorio.ecommercemanager.dto;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class PixEVPKeyDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String chave;
+
+    public PixEVPKeyDTO() {
+    }
+
+    public PixEVPKeyDTO(String chave) {
+        this.chave = chave;
+    }
+
+    public String getChave() {
+        return chave;
+    }
+
+    public void setChave(String chave) {
+        this.chave = chave;
+    }
+
+    @Override
+    public String toString() {
+        return "PixEVPKeyDTO {" +
+                "\n  chave: " + chave +
+                "\n}";
+    }
+}
