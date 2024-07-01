87bce71717fbff07c23a77496259414b29968ef6
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-07-01 19:36:06 -0300

    refactor: Refactor PaymentServiceImplTest class

--- src/test/java/com/rogeriogregorio/ecommercemanager/services/PaymentServiceImplTest.java
+++ src/test/java/com/rogeriogregorio/ecommercemanager/services/PaymentServiceImplTest.java
@@ -2,9 +2,13 @@ package com.rogeriogregorio.ecommercemanager.services;
 
 import com.rogeriogregorio.ecommercemanager.dto.requests.OrderItemRequest;
 import com.rogeriogregorio.ecommercemanager.dto.requests.PaymentRequest;
+import com.rogeriogregorio.ecommercemanager.dto.responses.OrderResponse;
 import com.rogeriogregorio.ecommercemanager.dto.responses.PaymentResponse;
 import com.rogeriogregorio.ecommercemanager.entities.*;
 import com.rogeriogregorio.ecommercemanager.entities.enums.OrderStatus;
+import com.rogeriogregorio.ecommercemanager.entities.enums.PaymentStatus;
+import com.rogeriogregorio.ecommercemanager.entities.enums.PaymentType;
+import com.rogeriogregorio.ecommercemanager.entities.enums.UserRole;
 import com.rogeriogregorio.ecommercemanager.exceptions.NotFoundException;
 import com.rogeriogregorio.ecommercemanager.exceptions.RepositoryException;
 import com.rogeriogregorio.ecommercemanager.mail.MailService;
@@ -23,12 +27,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -66,43 +71,76 @@ class PaymentServiceImplTest {
     @InjectMocks
     private PaymentServiceImpl paymentService;
 
+
+    private static User user;
+    private static Order order;
+    private static Payment payment;
+    private static PaymentRequest paymentRequest;
+    private static PaymentResponse paymentResponse;
+    private static DiscountCoupon discountCoupon;
+
     @BeforeEach
     void setUp() {
 
+        user = User.newBuilder()
+                .withId(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"))
+                .withName("Admin").withEmail("admin@email.com").withPhone("11912345678")
+                .withCpf("72482581052").withPassword("Password123$").withRole(UserRole.ADMIN)
+                .build();
 
+        order = Order.newBuilder()
+                .withId(1L)
+                .withClient(user)
+                .withMoment(Instant.now())
+                .withCoupon(discountCoupon)
+                .withOrderStatus(OrderStatus.WAITING_PAYMENT)
+                .withPayment(payment)
+                .build();
+
+        payment = Payment.newBuilder()
+                .withId(1L)
+                .withMoment(Instant.now())
+                .withOrder(order)
+                .withPaymentStatus(PaymentStatus.PROCESSING)
+                .withPaymentType(PaymentType.PIX)
+                .withTxId("b3f1b57e-ec0c-4b23-a6b2-647d2b176d74")
+                .withChargeLink("https://bank.com/paymentqrcode")
+                .build();
+
+        paymentRequest = new PaymentRequest(1L, PaymentType.PIX);
+
+        paymentResponse = new PaymentResponse(1L, Instant.now(), order, "b3f1b57e-ec0c-4b23-a6b2-647d2b176d74",
+                PaymentType.PIX, "https://bank.com/paymentqrcode", PaymentStatus.PROCESSING);
 
         MockitoAnnotations.openMocks(this);
         paymentService = new PaymentServiceImpl(paymentRepository, inventoryItemService, stockMovementService,
                 mailService, orderService, validators, paymentMethods, catchError, dataMapper);
     }
-//
-//    @Test
-//    @DisplayName("findAllPayments - Busca bem-sucedida retorna lista contendo um pagamento")
-//    void findAllPayments_SuccessfulSearch_ReturnsListResponse_OnePayment() {
-//        // Arrange
-//        User user = new User(1L, "João Silva", "joao@email.com", "11912345678", "senha123");
-//        Order order = new Order(1L, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user);
-//
-//        Payment payment = new Payment(1L, Instant.now(), order);
-//        List<Payment> paymentList = Collections.singletonList(payment);
-//
-//        PaymentResponse paymentResponse = new PaymentResponse(1L, Instant.now(), order);
-//        List<PaymentResponse> expectedResponses = Collections.singletonList(paymentResponse);
-//
-//        when(converter.toResponse(payment, PaymentResponse.class)).thenReturn(paymentResponse);
-//        when(paymentRepository.findAll()).thenReturn(paymentList);
-//
-//        // Act
-//        List<PaymentResponse> actualResponses = paymentService.findAllPayments();
-//
-//        // Assert
-//        assertEquals(expectedResponses.size(), actualResponses.size(), "Expected a list of responses with one payment");
-//        assertIterableEquals(expectedResponses, actualResponses, "Expected a list of responses with one payment");
-//
-//        verify(converter, times(1)).toResponse(payment, PaymentResponse.class);
-//        verify(paymentRepository, times(1)).findAll();
-//    }
-//
+
+    @Test
+    @DisplayName("findAllPayments - Busca bem-sucedida retorna lista contendo um pagamento")
+    void findAllPayments_SuccessfulSearch_ReturnsListResponse_OnePayment() {
+        // Arrange
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Payment> paymentList = Collections.singletonList(payment);
+        List<PaymentResponse> expectedResponses = Collections.singletonList(paymentResponse);
+        PageImpl<Payment> page = new PageImpl<>(paymentList, pageable, paymentList.size());
+
+        when(dataMapper.map(payment, PaymentResponse.class)).thenReturn(paymentResponse);
+        when(paymentRepository.findAll(pageable)).thenReturn(page);
+        when(catchError.run(any(CatchError.SafeFunction.class))).thenAnswer(invocation -> paymentRepository.findAll(pageable));
+
+        // Act
+        Page<PaymentResponse> actualResponses = paymentService.findAllPayments(pageable);
+
+        // Assert
+        assertEquals(expectedResponses.size(), actualResponses.getContent().size(), "Expected a list with one object");
+        assertIterableEquals(expectedResponses, actualResponses, "Expected and actual responses should be equal");
+        verify(dataMapper, times(1)).map(payment, PaymentResponse.class);
+        verify(paymentRepository, times(1)).findAll(pageable);
+        verify(catchError, times(1)).run(any(CatchError.SafeFunction.class));
+    }
+
 //    @Test
 //    @DisplayName("findAllPayments - Busca bem-sucedida retorna lista contendo múltiplos pagamentos")
 //    void findAllPayments_SuccessfulSearch_ReturnsListResponse_MultiplePayments() {
